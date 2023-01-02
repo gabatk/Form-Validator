8f74bf08bd014407f06eb8ee8d5fe9f9df6c84d4
gabatk <gabriela.trybuszek@gmail.com>
2023-01-02 22:33:59 +0100

    Operation of the send button in progress.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,30 @@ const pass2 = document.querySelector('#password2');
 const email = document.querySelector('#email');
 const popup = document.querySelector('#popup');
 
+const showError = (inputValue, msg) => {
+	// argument inputValue przechowuje nasze pojedyncze INPUTy;
+	// argument MSG przechowuje placeholder;
+};
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder);
+		}
+	});
+};
+
+// Argument INPUT z funkcji "checkForm" przechowuje tablicę z naszymi inputami;
+//  Argument EL odnosi się do każdej zmiennej, którą umieściliśmy w tablicy;
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault();
+
+	checkForm((username, pass, pass2, email));
+});
+
 clearBtn.addEventListener('click', e => {
-	e.preventDefault()
-	[(username, pass, pass2, email)].forEach(element => {
+	e.preventDefault()[(username, pass, pass2, email)].forEach(element => {
 		element.value = '';
 	});
 });
